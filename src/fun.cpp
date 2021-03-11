25cd9f2aa131fa8e045fbc16b6081a11e2c534b7
FoxedFox <71232184+FoxedFox@users.noreply.github.com>
2021-03-11 11:20:29 +0300

    updated2 fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,100 +1,100 @@
 #include "fun.h"
-#include <iostream>
-#include <cctype>
-#include <vector>
-std::vector<std::pair<const char*, const char*>> split(const char* str, char sep = ' ') 
+unsigned int faStr1(const char* str)
 {
-    bool on_word = false;
-    const char* StrStart = nullptr;
-    const char* StrEnd = nullptr;
-    std::vector<std::pair<const char*, const char*>> result;
-    int i = 0;
-    for (; str[i] != '\0'; i++) 
-    {
-        if (str[i] != sep && !on_word) 
-        {
-            on_word = true;
-            StrStart = str + i;
-        }
-        else if (str[i] == sep && on_word) 
-        {
-            on_word = false;
-            StrEnd = str + i;
-            result.emplace_back(StrStart, str + i);
-            StrStart = StrEnd = nullptr;
-        }
-        else 
-        {
-            continue;
-        }
-    }
-
-    if (StrStart != nullptr && StrEnd == nullptr) 
-    {
-        result.emplace_back(StrStart, str + i);
-    }
-    return result;
+	int i, count, pos, sym=0;
+	i = 0;
+	count = 0;
+	pos = 0;
+	while (str[i] != '\0')
+	{
+		if (pos == 0 && str[i] != 32)
+		{
+			if ( str[i] < 48 || str[i] > 57) 
+			sym= 1;
+			pos = 1;
+		}
+		else if (pos == 1 && str[i] == 32)
+		{
+			if (sym== 1) 
+			count++;
+			sym= 0;
+			pos = 0;
+		}
+		else if (pos == 1 && str[i] != 32 && sym== 1)
+		{
+			if (str[i] >= 48 && str[i] <= 57) 
+				sym = 0;
+		}
+		i++;
+	}
+	if (pos == 1 && sym== 1) 
+		count++;
+	return count;
 }
 
-unsigned int faStr1(const char* str) 
+unsigned int faStr2(const char* str)
 {
-    auto words = split(str);
-    int result = 0;
-    auto process_word = [&result](const char* begin, const char* end) 
-    {
-        for (; begin != end; begin++) 
-        {
-            if (std::isdigit(*begin)) 
-            {
-                return;
-            }
-        }
-        result++;
-    };
-    for (auto word : words) 
-    {
-        process_word(word.first, word.second);
-    }
-    return result;
+	int i, count, pos, sym = 0;
+	i = 0;
+	count = 0;
+	pos = 0;
+	while (str[i] != '\0')
+	{
+		if (pos == 0 && str[i] >= 65 && str[i] <= 90)
+		{
+			pos = 1;
+			sym = 1;
+		}
+		else if (pos == 1 && str[i] == 32)
+		{
+			if (sym== 1) count++;
+			sym= 0;
+			pos = 0;
+		}
+		else if (pos == 1 && str[i] != 32 &&sym== 1)
+		{
+			if (str[i] >= 65 && str[i] <= 90)
+				sym= 0;
+			if (str[i] < 65 ||str[i] > 122 )
+				sym= 0;
+		}
+		i++;
+	}
+	if (pos == 1 &&sym== 1) count++;
+	return count;
 }
 
-unsigned int faStr2(const char* str) 
+unsigned int faStr3(const char* str)
 {
-    auto words = split(str);
-    int result = 0;
-    auto process_word = [&result](const char* begin, const char* end) 
-    {
-        if (!std::isupper(*(begin++))) 
-        {
-            return;
-        }
-        for (; begin != end; begin++) 
-        {
-            if (!std::islower(*begin)) 
-            {
-                return;
-            }
-        }
-        result++;
-    };
-    for (auto word : words) 
-    {
-        process_word(word.first, word.second);
-    }
-    return result;
-}
-
-unsigned int faStr3(const char* str) 
-{
-    auto words = split(str);
-    int result = 0;
-    auto process_word = [&result](const char* begin, const char* end) 
-    {
-        result += end - begin;
-    };
-    for (auto word : words) 
-    {
-        process_word(word.first, word.second);
-    }
-    return words.size() ? (result / (float)words.size() + 0.5) : 0;
+	int length = 0;
+	int i, count, pos, sym = 0;
+	i = 0;
+	count = 0;
+	pos = 0;
+	 int mid_length = 0;
+	float sum = 0;
+	while (str[i] != '\0')
+	{
+		if (pos == 0 && str[i] != 32)
+		{
+			count++;
+			pos = 1;
+			length++;
+		}
+		else if (pos == 1 && str[i] == 32)
+		{
+			sum = sum + length;
+			pos = 0;
+			length = 0;
+		}
+		else if (pos == 1 && str[i] != 32) 
+			length++;
+		i++;
+	}
+	if (pos == 1) sum = sum + length;
+	sum = sum / (float)count;
+	mid_length = sum;
+	if (sum - (float)mid_length >= 0.5) 
+		mid_length++;
+	return mid_length;
 }
